2714d02c992172af5fd35079a4f9168b154b7b00
Nicolas Coderre <nicolas.coderre@gmail.com>
2017-01-21 11:50:03 -0800

    basic data types for spawning

---
+++ Assets/Scripts/Utils/MonobehaviourSingleton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class MonoBehaviourSingleton<T> : MonoBehaviour where T : class {
+
+    private static T instance;
+
+    public static T Instance { 
+        get {
+            if (instance == null)
+            {
+                var r = FindObjectOfType(typeof(T));
+                var i = r as T;
+                return i;
+            }
+            else
+            {
+                return instance;
+            }
+        }
+    }
+    // Use this for initialization
+    protected void Awake () {
+        instance = this as T;
+	}
+
+    protected void OnDestroy()
+    {
+        instance = null;
+    }
+	
+}
