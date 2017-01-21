aa362916a056d0d709cf48fbff34ac8ad46cd892
Alex Sink <alexsink@yahoo.com>
2017-01-21 15:27:48 -0800

    When base dies, game ends, and loads game end scene (unloading all the main scenes)

--- Assets/Enemies/Scripts/BaseStats.cs
+++ Assets/Enemies/Scripts/BaseStats.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseStats : Stats {
+
     public delegate void ObjectHit(GameObject target, AttackType attack);
     public static event ObjectHit Hit;
     // Use this for initialization
