9f7b9cd88599c0a83e8aaede21ba70a13073aa42
Nicolas Coderre <nicolas.coderre@gmail.com>
2017-01-21 17:32:25 -0800

    screen to ray

---
+++ Assets/SonarView/HUD/DotClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DotClick : MonoBehaviour, IPointerDownHandler {
+
+	private Vector3 clickMousePos;
+	private Vector3 clickPos;
+	private bool mouseDown;
+	public GameObject itemPrefab;
+	public Transform ParentPanel;
+
+	public void OnPointerDown(PointerEventData ped) 
+	{
+		mouseDown = true;
+		clickPos = transform.position;
+		clickMousePos = Input.mousePosition;
+		HUDController.Instance.OnPanelClick(ped);
+
+	}
+
+}
