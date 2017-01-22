d19bb8bdeae7c8c09a9e52b2d04316e67cd7cffc
Jono <jono.forbes@gmail.com>
2017-01-21 16:09:24 -0800

    Added cannon. Pow pow pow.

---
+++ Assets/Shooter/Cannon/Scripts/CannonHandle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CannonHandle : MonoBehaviour
+{
+    [SerializeField] private Transform m_YPivot;
+    [SerializeField] private Transform m_XPivot;
+
+    public float m_RotationSpeed = 10f;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(DragHandle());
+        }
+    }
+
+    IEnumerator DragHandle()
+    {
+        var lastHandPosition = VRPlayer.rightHand.position;
+        while (!Input.GetButtonUp("Fire1"))
+        {
+            m_YPivot.Rotate(Vector3.up, (lastHandPosition - VRPlayer.rightHand.position).x * m_RotationSpeed);
+            m_XPivot.Rotate(Vector3.left, (lastHandPosition - VRPlayer.rightHand.position).y * m_RotationSpeed);
+            lastHandPosition = VRPlayer.rightHand.position;
+            yield return null;
+        }
+    }
+}
--- Assets/Shooter/Player/Scripts/VRPlayer.cs
+++ Assets/Shooter/Player/Scripts/VRPlayer.cs
@@ -6,6 +6,9 @@ public class VRPlayer : MonoBehaviour
 {
     private static VRPlayer s_Instance;
 
+    public static Transform leftHand { get { return s_Instance.m_LeftHand; } }
+    public static Transform rightHand { get { return s_Instance.m_RightHand; } }
+
     [Header("Settings")]
     [SerializeField] private ProjectileWeapon m_ActiveWeapon;
     [SerializeField] private float m_TeleportFadeDuration = 0.5f;
@@ -22,6 +25,9 @@ public class VRPlayer : MonoBehaviour
     [SerializeField]
     private Transform m_TeleSpot3;
 
+    private readonly Color m_Opaque = new Color(0, 0, 0, 1);
+    private readonly Color m_Invisible = new Color(0, 0, 0, 0);
+
     void Awake()
     {
         s_Instance = this;
@@ -34,7 +40,7 @@ public class VRPlayer : MonoBehaviour
         m_LeftHand.localRotation = InputTracking.GetLocalRotation(VRNode.LeftHand);
         m_RightHand.localPosition = InputTracking.GetLocalPosition(VRNode.RightHand);
         m_RightHand.localRotation = InputTracking.GetLocalRotation(VRNode.RightHand);
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire2"))
         {
             m_ActiveWeapon.Fire(m_RightHand.position, m_RightHand.forward);
         }
@@ -58,23 +64,28 @@ public class VRPlayer : MonoBehaviour
         s_Instance.StartCoroutine(s_Instance.TeleportWithFade(target.position));
     }
 
+    private static Color FadeToBlack(float alpha)
+    {
+        return new Color(0, 0, 0, alpha);
+    }
+
     IEnumerator TeleportWithFade(Vector3 targetPosition)
     {
         var t = 0f;
         while (t < 1)
         {
-            m_FadeMaterial.color = new Color(0, 0, 0, t);
+            m_FadeMaterial.color = FadeToBlack(t);
             t += Time.unscaledDeltaTime * (1 / m_TeleportFadeDuration);
             yield return null;
         }
-        m_FadeMaterial.color = new Color(0,0,0,1);
+        m_FadeMaterial.color = m_Opaque;
         transform.position = targetPosition;
         while (t > 0)
         {
-            m_FadeMaterial.color = new Color(0, 0, 0, t);
+            m_FadeMaterial.color = FadeToBlack(t);
             t -= Time.unscaledDeltaTime * (1 / m_TeleportFadeDuration);
             yield return null;
         }
-        m_FadeMaterial.color = new Color(0,0,0,0);
+        m_FadeMaterial.color = m_Invisible;
     }
 }
