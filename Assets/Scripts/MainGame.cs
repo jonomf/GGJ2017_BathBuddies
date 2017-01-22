8d485e82e8ff8160f93198ec18f8d2872c1d04b8
Alex Sink <alexsink@yahoo.com>
2017-01-22 11:34:53 -0800

    speed up time by 10x by default

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -33,7 +33,7 @@ public class MainGame : MonoBehaviourSingleton<MainGame> {
 		return Time.time;
 	}
 
-	[SerializeField] private float _multipleToSpeedUpTimeBy = 3f;
+	[SerializeField] private float _multipleToSpeedUpTimeBy = 10f;
 
 	[ContextMenu("Make invincible, kill current wave, speed up time")]
 	private void TestDesign()
